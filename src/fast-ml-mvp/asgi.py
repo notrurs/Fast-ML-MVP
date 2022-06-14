c96ac2bb4f405541db9f168b5da04f2955e22d1a
notrurs <fursik111@gmail.com>
2022-06-15 00:20:26 +0300

    Rename django project to fast-ml-mvp

--- src/fast-ml-mvp/asgi.py
+++ src/fast-ml-mvp/asgi.py
@@ -1,12 +1,3 @@
-"""
-ASGI config for mnist_demo project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
-"""
-
 import os
 
 from django.core.asgi import get_asgi_application
@@ -16,7 +7,7 @@ from channels.auth import AuthMiddlewareStack
 import demo.routing
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mnist_demo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fast-ml-mvp.settings')
 
 
 application = ProtocolTypeRouter({
