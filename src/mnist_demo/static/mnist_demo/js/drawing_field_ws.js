09572c384a45a0d3942fcaa37f0d76287cff6378
notrurs <fursik111@gmail.com>
2022-06-17 01:19:50 +0300

    Remove onmessage from event listener to outside

--- src/mnist_demo/static/mnist_demo/js/drawing_field_ws.js
+++ src/mnist_demo/static/mnist_demo/js/drawing_field_ws.js
@@ -1,17 +1,17 @@
-// WebSocket
+// WebSocket init
 let url = 'ws://' + window.location.host + '/ws_draw/';
 const appSocket = new WebSocket(url);
 
-// WS post logic
+// What to do when websocket gets message from server
+appSocket.onmessage = function (e) {
+    let response = JSON.parse(e.data);
+    updatePredictionTable(response)
+}
+
+// Canvas mousemove listener
 canvas.addEventListener("mousemove", function (e) {
     if (draw === true) {
         let json_image = getJsonImage()
-
-        appSocket.onmessage = function (e) {
-            let response = JSON.parse(e.data);
-            updatePredictionTable(response)
-        }
-
         appSocket.send(json_image);
     }
 });
