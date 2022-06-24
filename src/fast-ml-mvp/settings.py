3f5d85a41c4efd161ed2dcff00a122576250a31b
notrurs <fursik111@gmail.com>
2022-06-24 10:31:21 +0300

    Add environment variables

--- src/fast-ml-mvp/settings.py
+++ src/fast-ml-mvp/settings.py
@@ -1,3 +1,5 @@
+from os import getenv
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -8,15 +10,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gvg1%wz@r#sc&1^*fx=o75q5_#12ta3*4nn!7hf)$(gxm2+w_)'
+SECRET_KEY = getenv('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv('DJANGO_DEBUG')
 
 ALLOWED_HOSTS = [
     'app'
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    f'http://{getenv("DJANGO_ALLOWED_HOST")}'
+]
 
 # Application definition
 
