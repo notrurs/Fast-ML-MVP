8917e54802d3cee43be1d29d63f3b6ef30b9d6aa
notrurs <fursik111@gmail.com>
2022-06-24 15:16:38 +0300

    Comment admin path

--- src/fast-ml-mvp/urls.py
+++ src/fast-ml-mvp/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('', include('mnist_demo.urls'))
 ]
